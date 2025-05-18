8151146300fb0228f67d74b9f79970279fe8ef2d
SahilYadav220 <114868955+SahilYadav220@users.noreply.github.com>
2025-05-18 23:23:29 +0530

    done3

--- server.js
+++ server.js
@@ -129,8 +129,8 @@ import menuroutes from './routes/menuitems.js';
 app.use('/menu',menuroutes);
 
 app.use('/person',personroutes);
-//const PORT=process.env.PORT || 4000;
+const PORT=process.env.PORT || 4000;
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on http://localhost:4000');
 });
